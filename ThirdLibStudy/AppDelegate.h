d8fa841eb908b95bb4dd0aecce4524e671c09323
dujia <815319775@qq.com>
2016-07-30 16:53:26 +0800

    first commit

---
+++ ThirdLibStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThirdLibStudy
+//
+//  Created by dujia on 16/7/30.
+//  Copyright © 2016年 dujia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThirdLibStudy/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ThirdLibStudy
+//
+//  Created by dujia on 16/7/30.
+//  Copyright © 2016年 dujia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
